ef2b45ebada2b51854e4e7f55234aa20728179a0
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2025-05-08 18:19:46 +0300

    Get current OS type, name and version

--- shell_vnc.sh
+++ shell_vnc.sh
@@ -15,6 +15,21 @@ SHELL_VNC_PATH="$(dirname "$0")" || return "$?" 2> /dev/null || exit "$?"
 
 export _SHELL_VNC_RETURN_CODE_WHEN_FILE_IS_ALREADY_SOURCED=238
 
+export _SHELL_VNC_CURRENT_OS_TYPE="${_SHELL_VNC_CURRENT_OS_TYPE}"
+export _SHELL_VNC_OS_TYPE_WINDOWS="windows"
+export _SHELL_VNC_OS_TYPE_LINUX="linux"
+export _SHELL_VNC_OS_TYPE_MACOS="macos"
+
+export _SHELL_VNC_CURRENT_OS_NAME="${_SHELL_VNC_CURRENT_OS_NAME}"
+export _SHELL_VNC_OS_NAME_WINDOWS="windows"
+export _SHELL_VNC_OS_NAME_TERMUX="termux"
+export _SHELL_VNC_OS_NAME_ARCH="arch"
+export _SHELL_VNC_OS_NAME_FEDORA="fedora"
+export _SHELL_VNC_OS_NAME_DEBIAN="debian"
+export _SHELL_VNC_OS_NAME_MACOS="macos"
+
+export _SHELL_VNC_CURRENT_OS_VERSION="${_SHELL_VNC_CURRENT_OS_VERSION}"
+
 shell_vnc_return_0_if_already_sourced() {
   local return_code="$?"
 
@@ -91,20 +106,78 @@ export -f shell_vnc_required_after_function
 shell_vnc_required_before_imports "${BASH_SOURCE[0]}" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/messages/_constants.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/messages/shell_vnc_print_info.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
+. "./scripts/messages/shell_vnc_print_info_increase_prefix.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/messages/shell_vnc_print_error.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
+. "./scripts/messages/shell_vnc_print_text.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
+. "./scripts/messages/shell_vnc_print_success_decrease_prefix.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/shell_vnc_install.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/shell_vnc_uninstall.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 . "./scripts/shell_vnc_update.sh" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 shell_vnc_required_after_imports "${BASH_SOURCE[0]}" || shell_vnc_return_0_if_already_sourced || return "$?" 2> /dev/null || exit "$?"
 
+shell_vnc_init_current_os_type_and_name() {
+  shell_vnc_print_info_increase_prefix "Initializing current OS type and name..." || return "$?"
+
+  local current_kernel_name
+  current_kernel_name="$(uname -s)" || return "$?"
+
+  if [ -n "${MSYSTEM}" ]; then
+    _SHELL_VNC_CURRENT_OS_TYPE="${_SHELL_VNC_OS_TYPE_WINDOWS}"
+    _SHELL_VNC_CURRENT_OS_NAME="${_SHELL_VNC_OS_NAME_WINDOWS}"
+  elif [ "${current_kernel_name}" = "Linux" ]; then
+    _SHELL_VNC_CURRENT_OS_TYPE="${_SHELL_VNC_OS_TYPE_LINUX}"
+
+    # For Termux there is no "/etc/os-release" file, so we need to check it separately
+    if [ -n "${TERMUX_VERSION}" ]; then
+      _SHELL_VNC_CURRENT_OS_NAME="${_SHELL_VNC_OS_NAME_TERMUX}"
+      _SHELL_VNC_CURRENT_OS_VERSION="${TERMUX_VERSION}"
+    else
+      if [ ! -f "/etc/os-release" ]; then
+        shell_vnc_print_error "File \"/etc/os-release\" not found - probably, \"_n2038_init_current_os_name\" is not implemented for your OS." || return "$?"
+        return 1
+      fi
+
+      _SHELL_VNC_CURRENT_OS_NAME="$(sed -n 's/^ID=//p' /etc/os-release)" || return "$?"
+
+      if [ -z "${_SHELL_VNC_CURRENT_OS_NAME}" ]; then
+        shell_vnc_print_error "Could not determine the current OS name!" || return "$?"
+        return 1
+      fi
+
+      if [ "${_SHELL_VNC_CURRENT_OS_NAME}" = "${_SHELL_VNC_OS_NAME_ARCH}" ]; then
+        # There is no version for Arch
+        _SHELL_VNC_CURRENT_OS_VERSION=""
+      else
+        _SHELL_VNC_CURRENT_OS_VERSION="$(sed -En 's/^VERSION_ID="?([^"]+)"?/\1/p' /etc/os-release)" || return "$?"
+      fi
+    fi
+  elif [ "${current_kernel_name}" = "Darwin" ]; then
+    _SHELL_VNC_CURRENT_OS_TYPE="${_SHELL_VNC_OS_TYPE_MACOS}"
+    _SHELL_VNC_CURRENT_OS_NAME="${_SHELL_VNC_OS_NAME_MACOS}"
+    shell_vnc_print_error "Getting OS version is not implemented in \"_n2038_init_current_os_version\" for \"${_SHELL_VNC_CURRENT_OS_NAME}\"!" || return "$?"
+    return 1
+  else
+    shell_vnc_print_error "Could not determine the current OS type!" || return "$?"
+    return 1
+  fi
+
+  shell_vnc_print_text "Current OS type: \"${c_highlight}${_SHELL_VNC_CURRENT_OS_TYPE}${c_return}\"." || return "$?"
+  shell_vnc_print_text "Current OS name: \"${c_highlight}${_SHELL_VNC_CURRENT_OS_NAME}${c_return}\"." || return "$?"
+  shell_vnc_print_text "Current OS version: \"${c_highlight}${_SHELL_VNC_CURRENT_OS_VERSION}${c_return}\"." || return "$?"
+  shell_vnc_print_success_decrease_prefix "Initializing current OS type and name: success!" || return "$?"
+}
+
 shell_vnc() {
+  shell_vnc_print_info_increase_prefix "Running scripts..." || return "$?"
+
   if [ "$#" -lt 1 ]; then
     shell_vnc_print_error "Usage: ${c_highlight}${FUNCNAME[0]} <install|uninstall|update>${c_return}" || return "$?"
     return 1
   fi
 
-  local action="$1" && shift
+  shell_vnc_init_current_os_type_and_name || return "$?"
 
+  local action="$1" && shift
   if [ "${action}" = "install" ]; then
     shell_vnc_install "$@" || return "$?"
   elif [ "${action}" = "uninstall" ]; then
@@ -115,6 +188,8 @@ shell_vnc() {
     shell_vnc_print_error "Unknown action: \"${c_highlight}${action}${c_return}\"." || return "$?"
     return 1
   fi
+
+  shell_vnc_print_success_decrease_prefix "Running scripts: success!" || return "$?"
 }
 
 shell_vnc_required_after_function "${BASH_SOURCE[0]}" "$@" || return "$?" 2> /dev/null || exit "$?"
